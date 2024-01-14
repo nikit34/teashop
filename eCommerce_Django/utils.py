1a365f45f4fb5fe0ef179b5169499c2fc2e0e687
nikit34 <permikov134@mail.ru>
2024-01-14 02:21:15 +0000

    Fix tag generator

--- eCommerce_Django/utils.py
+++ eCommerce_Django/utils.py
@@ -74,7 +74,7 @@ def unique_slug_generator(instance, new_slug=None):
     if new_slug is not None:
         slug = new_slug
     else:
-        slug = slugify(instance.title)
+        slug = slugify(instance.title, True)
 
     Klass = instance.__class__
     qs_exists = Klass.objects.filter(slug=slug).exists()
