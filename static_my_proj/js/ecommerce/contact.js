82bf41f956eb9584712e0768057ec691179f3aef
nikit34 <permikov134@mail.ru>
2024-02-25 20:37:41 +0000

    Separate on contact

---
+++ static_my_proj/js/ecommerce/contact.js
@@ -0,0 +1,81 @@
+import { localization } from './base.js';
+
+
+$(document).ready(function () {
+  let lang = localization();
+
+  var contactForm = $(".contact-form");
+  var contactFormMethod = contactForm.attr("method");
+  var contactFormEndpoint = contactForm.attr("action");
+  function displaySubmitting(submitBtn, defaultText, doSubmit) {
+    if (doSubmit) {
+      submitBtn.addClass("disabled");
+      var fragment;
+      switch (lang) {
+        case "en":
+          fragment = '<i class="fa fa-spin fa-spinner"></i>Sending...';
+          break;
+        case "ru":
+          fragment = '<i class="fa fa-spin fa-spinner"></i>Отправление...';
+          break;
+        case "pt":
+          fragment = '<i class="fa fa-spin fa-spinner"></i>Enviando...';
+          break;
+        default:
+          fragment = '<i class="fa fa-spin fa-spinner"></i>Undefined langueges in js';
+      }
+      submitBtn.html(fragment);
+    } else {
+      submitBtn.removeClass("disabled");
+      submitBtn.html(defaultText);
+    }
+  }
+  contactForm.submit(function (event) {
+    event.preventDefault();
+    var contactFormSubmitBtn = contactForm.find('[type="submit"]');
+    var contactFormSubmitBtnTxt = contactFormSubmitBtn.text();
+    var contactFormData = contactForm.serialize();
+    var thisForm = $(this);
+    displaySubmitting(contactFormSubmitBtn, "", true);
+    let lang = localization();
+    $.ajax({
+      method: contactFormMethod,
+      url: contactFormEndpoint,
+      data: contactFormData,
+      success: function (data) {
+        contactForm[0].reset();
+        $.alert({
+          title: "Success!",
+          content: data.message,
+          theme: "modern",
+        });
+        setTimeout(function () {
+          displaySubmitting(
+            contactFormSubmitBtn,
+            contactFormSubmitBtnTxt,
+            false
+          );
+        }, 500);
+      },
+      error: function (error) {
+        var jsonData = error.responseJSON;
+        var msg = "";
+        $.each(jsonData, function (key, value) {
+          msg += key + ": " + value[0].message + "<br/>";
+        });
+        $.alert({
+          title: "Break!",
+          content: msg,
+          theme: "modern",
+        });
+        setTimeout(function () {
+          displaySubmitting(
+            contactFormSubmitBtn,
+            contactFormSubmitBtnTxt,
+            false
+          );
+        }, 500);
+      },
+    });
+  });
+});
