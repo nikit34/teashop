f469280a555a2be7147ce5da5b86ac43848cbe15
nikit34 <permikov134@mail.ru>
2024-02-25 04:50:54 +0000

    Fix ecommerce.cart.js

--- static_my_proj/js/ecommerce.cart.js
+++ static_my_proj/js/ecommerce.cart.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
   var decrementBtn = $('.decrement-btn')
   decrementBtn.click(function(event) {
     event.preventDefault();
-    var currentQuantity = parseInt($('.currentQuantity').val());
     var thisBtn = $(this);
     var productId = thisBtn.data('product-id');
     var actionEndpoint = thisBtn.attr("data-endpoint");
     var method = thisBtn.data('method');
+    var currentQuantity = parseInt($('.currentQuantity[data-product-id="' + productId + '"]').val());
 
     var newQuantity = currentQuantity - 1;
     $.ajax({
@@ -18,12 +18,7 @@ $(document).ready(function () {
         'new_quantity': newQuantity
       },
       success: function(data) {
-        if (data.removed) {
-          $('.currentQuantity[data-product-id="' + productId + '"]').val(newQuantity);
-          console.log('Quantity decrement successfully');
-        } else {
-          console.log('Failed to update quantity:', data.error);
-        }
+        $('.currentQuantity[data-product-id="' + productId + '"]').val(newQuantity);
       },
       error: function(xhr, errmsg, err) {
         console.log(xhr.status + ': ' + xhr.responseText);
@@ -34,11 +29,11 @@ $(document).ready(function () {
   var incrementBtn = $('.increment-btn')
   incrementBtn.click(function(event) {
     event.preventDefault();
-    var currentQuantity = parseInt($('.currentQuantity').val());
     var thisBtn = $(this);
     var productId = thisBtn.data('product-id');
     var actionEndpoint = thisBtn.attr("data-endpoint");
     var method = thisBtn.data('method');
+    var currentQuantity = parseInt($('.currentQuantity[data-product-id="' + productId + '"]').val());
 
     var newQuantity = currentQuantity + 1;
     $.ajax({
@@ -49,12 +44,7 @@ $(document).ready(function () {
         'new_quantity': newQuantity
       },
       success: function(data) {
-        if (data.added) {
-          $('.currentQuantity[data-product-id="' + productId + '"]').val(newQuantity);
-          console.log('Quantity increment successfully');
-        } else {
-          console.log('Failed to update quantity:', data.error);
-        }
+        $('.currentQuantity[data-product-id="' + productId + '"]').val(newQuantity);
       },
       error: function(xhr, errmsg, err) {
         console.log(xhr.status + ': ' + xhr.responseText);
