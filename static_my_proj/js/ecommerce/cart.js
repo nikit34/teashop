dc7e21d612ac398b105192999dac23c27ec74b88
nikit34 <permikov134@mail.ru>
2024-02-26 23:39:22 +0000

    Move refreshCart function

--- static_my_proj/js/ecommerce/cart.js
+++ static_my_proj/js/ecommerce/cart.js
@@ -59,4 +59,59 @@ $(document).ready(function () {
       }
     });
   });
+
+  function refreshCart() {
+    var cartTable = $(".cart-table");
+    var cartBody = cartTable.find(".cart-body");
+    var productRows = cartBody.find(".cart-product");
+    var currentUrl = window.location.href;
+
+    var refreshCartUrl = "/api/cart/";
+    var refreshCartMethod = "GET";
+    var data = {};
+    $.ajax({
+      url: refreshCartUrl,
+      method: refreshCartMethod,
+      data: data,
+      success: function (data) {
+        var hiddenCartItemRemoveForm = $(".cart-item-remove-form");
+        if (data.products.length > 0) {
+          productRows.html(" ");
+          var i = data.products.length;
+          $.each(data.products, function (index, value) {
+            var newCartItemRemove = hiddenCartItemRemoveForm.clone();
+            newCartItemRemove.css("display", "block");
+            newCartItemRemove.find(".cart-item-product-id").val(value.id);
+            cartBody.prepend(
+              '<tr><th scope="row">' +
+                i +
+                '</th><td><a href="' +
+                value.url +
+                '">' +
+                value.name +
+                "</a>" +
+                newCartItemRemove.html() +
+                "</td><td>" +
+                value.price +
+                "</tb></tr>"
+            );
+            i--;
+          });
+          cartBody.find(".cart-subtotal").text(data.subtotal);
+          cartBody.find(".cart-total").text(data.total);
+        } else {
+          window.location.href = currentUrl;
+        }
+      },
+      error: function (errorData) {
+        $.alert({
+          title: "Break!",
+          content: "An error occurred",
+          theme: "modern",
+        });
+      },
+    });
+  }
+
+  window.refreshCart = refreshCart;
 });
