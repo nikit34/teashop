08bb1ea9e56d57f7cced83f69dab1e8024daa423
nikit34 <permikov134@mail.ru>
2024-03-05 19:12:19 +0000

    Refactoring

--- analytics/models.py
+++ analytics/models.py
@@ -9,6 +9,7 @@ from accounts.signals import user_logged_in
 from .signals import object_viewed_signal
 from .utils import get_client_ip
 
+
 User = settings.AUTH_USER_MODEL
 
 FORCE_SESSION_TO_ONE = getattr(settings, 'FORCE_SESSION_TO_ONE', False)
@@ -46,7 +47,7 @@ class ObjectViewed(models.Model):
     objects = ObjectViewedManager()
 
     def __str__(self):
-        return '%s viewed on %s' %(self.content_object, self.timestamp)
+        return '%s viewed on %s' % (self.content_object, self.timestamp)
 
     class Meta:
         ordering = ['-timestamp']
@@ -60,10 +61,10 @@ def object_viewed_receiver(sender, instance, request, *args, **kwargs):
     if request.user.is_authenticated:
         user = request.user
     new_view_obj = ObjectViewed.objects.create(
-        user = user,
-        content_type = c_type,
-        object_id = instance.id,
-        ip_address = get_client_ip(request)
+        user=user,
+        content_type=c_type,
+        object_id=instance.id,
+        ip_address=get_client_ip(request)
     )
 
 
@@ -99,16 +100,18 @@ def post_save_session_receiver(sender, instance, created, *args, **kwargs):
     if not instance.active and not instance.ended:
         instance.end_session()
 
+
 if FORCE_SESSION_TO_ONE:
     post_save.connect(post_save_session_receiver, sender=UserSession)
 
 
 def post_save_user_changed_receiver(sender, instance, created, *args, **kwargs):
     if not created:
-        if instance.is_active == False:
+        if not instance.is_active:
             qs = UserSession.objects.filter(user=instance.user, ended=False, active=False)
             for i in qs:
                 i.end_session()
+
 
 if FORCE_INACTIVE_USER_ENDSESSION:
     post_save.connect(post_save_user_changed_receiver, sender=User)
