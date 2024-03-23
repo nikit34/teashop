8dc38255f054aaa3847703b50f84bc1132ab1e03
nikit34 <permikov134@mail.ru>
2024-03-23 22:05:24 +0000

    Move test on views

--- products/tests/test_models.py
+++ products/tests/test_models.py
@@ -53,19 +53,3 @@ class ProductFileModelTestCase(TestCase):
         self.assertEqual(product_file.product, self.product)
         self.assertEqual(product_file.display_name, "Test File")
 
-
-class ProductViewTestCase(TestCase):
-    def setUp(self):
-        self.product = Product.objects.create(
-            title="Test Product",
-            description="This is a test product",
-            price=50.00,
-            featured=True,
-            active=True,
-            quantity=10
-        )
-
-    def test_product_detail_view(self):
-        response = self.client.get(reverse("products:detail", kwargs={"slug": self.product.slug}))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Test Product")
