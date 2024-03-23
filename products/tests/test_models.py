1079dcd1e492343af7134605d6d72a17fdee876d
nikit34 <permikov134@mail.ru>
2024-03-23 22:03:32 +0000

    Rewrite ProductModelTest

--- products/tests/test_models.py
+++ products/tests/test_models.py
@@ -1,25 +1,35 @@
+from decimal import Decimal
+
 from django.test import TestCase
 from django.urls import reverse
 
 from products.models import Product, ProductFile
 
 
-class ProductModelTestCase(TestCase):
-    def setUp(self):
-        self.product = Product.objects.create(
-            title="Test Product",
-            description="This is a test product",
-            price=50.00,
+class ProductModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Product.objects.create(
+            title='Test Product',
+            description='This is a test product',
+            price=10.99,
             featured=True,
             active=True,
             quantity=10
         )
 
-    def test_product_creation(self):
-        product = Product.objects.get(title="Test Product")
+    def test_get_absolute_url(self):
+        product = Product.objects.get(title='Test Product')
+        self.assertEqual(product.get_absolute_url(), reverse('products:detail', kwargs={'slug': product.slug}))
+
+    def test_product_str_method(self):
+        product = Product.objects.get(title='Test Product')
+        self.assertEqual(str(product), 'Test Product')
         self.assertEqual(product.slug, "test-product")
-        self.assertEqual(product.price, 50.00)
+        self.assertEqual(product.price, Decimal('10.99') )
         self.assertTrue(product.active)
+        self.assertTrue(product.featured)
+        self.assertEqual(product.quantity, 10)
 
 
 class ProductFileModelTestCase(TestCase):
