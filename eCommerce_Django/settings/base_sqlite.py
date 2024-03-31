940081303a6704a931876b96989419699d1cad9f
nikit34 <permikov134@mail.ru>
2024-03-31 23:15:29 +0100

    Add test db

--- eCommerce_Django/settings/base_sqlite.py
+++ eCommerce_Django/settings/base_sqlite.py
@@ -121,7 +121,7 @@ DATABASES = {
     },
     'TEST': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
     },
 }
 
