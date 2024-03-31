c1741770721b3f99addd7029b90686572ef29be9
nikit34 <permikov134@mail.ru>
2024-03-31 23:14:24 +0100

    Add test db

--- eCommerce_Django/settings/base_sqlite.py
+++ eCommerce_Django/settings/base_sqlite.py
@@ -118,7 +118,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    'TEST': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
