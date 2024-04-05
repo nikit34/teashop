181e42af309611707d98781a87c7d8fe43616f5e
nikit34 <permikov134@mail.ru>
2024-04-05 01:03:54 +0100

    Add grammary

--- products/models.py
+++ products/models.py
@@ -74,6 +74,7 @@ class Product(models.Model):
     slug = models.SlugField(blank=True, unique=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    grammage = models.CharField(max_length=20, blank=True, null=True)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     featured = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
