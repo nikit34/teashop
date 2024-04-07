86a62e1af1de481822104e99ba246e34ec9f0efe
nikit34 <permikov134@mail.ru>
2024-04-07 17:46:18 +0100

    Move urls

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
+from analytics.views import SalesView, SalesAjaxView
 from carts.views import cart_detail_api_view, checkout_api_view
 
 
 urlpatterns = [
     path('cart/', cart_detail_api_view, name='api-cart'),
     path('checkout/', checkout_api_view, name='api-checkout'),
+    path('sales/', SalesView.as_view(), name='sales-analytics'),
+    path('sales/data/', SalesAjaxView.as_view(), name='sales-analytics-data'),
 ]
